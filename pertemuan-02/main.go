917cc224d341ca8ac5653802cf82b6454acc363d
danennh11 <danendranayott@gmail.com>
2026-09-08 17:16:00 +0700

    pertemuan-02: Level 3 selesai

--- pertemuan-02/main.go
+++ pertemuan-02/main.go
@@ -23,7 +23,7 @@ func HitungTotalPesanan(qty []int, hargaSatuan []float64) float64 {
 
 func TerapkanPajak(total float64, tarifPajak float64) float64 {
 
-	return 0
+	return total + (total * tarifPajak)
 }
 
 func HitungDiskon(total float64) float64 {
